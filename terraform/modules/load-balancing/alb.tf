98f78302f86d50948ae60651fda47a4833701aef
mayorKHAY26 <mayor_omot@yahoo.com>
2026-06-01 22:10:20 -0400

    updated files

--- terraform/modules/load-balancing/alb.tf
+++ terraform/modules/load-balancing/alb.tf
@@ -22,7 +22,7 @@ resource "aws_lb_target_group" "frontend" {
   health_check {
     path                = "/"
     protocol            = "HTTP"
-    matcher             = "200"
+    matcher             = "200-499"
     interval            = 30
     timeout             = 5
     healthy_threshold   = 2
