4ffb637d2c1db12a08968b73c5c8c6476aa72c10
mayorKHAY26 <mayor_omot@yahoo.com>
2026-06-01 22:19:56 -0400

    Allow ALB hostname in Vite frontend

--- app/frontend/vite.config.js
+++ app/frontend/vite.config.js
@@ -3,10 +3,11 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react],
-
   server: {
     host: "0.0.0.0",
     port: 3000,
-    allowedHosts: "all"
+    allowedHosts: [
+      "inlook-webmail-komot-dev-alb-2013284388.us-east-1.elb.amazonaws.com"
+    ]
   }
 });
