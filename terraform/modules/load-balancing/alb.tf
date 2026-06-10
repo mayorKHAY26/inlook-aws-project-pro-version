a25fb32befd74a8c9d6891ec798d129b75725c17
mayorKHAY26 <mayor_omot@yahoo.com>
2026-06-10 17:10:30 -0400

    updated files

--- terraform/modules/load-balancing/alb.tf
+++ terraform/modules/load-balancing/alb.tf
@@ -40,6 +40,34 @@ resource "aws_lb_target_group" "frontend" {
   }
 }
 
+resource "aws_lb_target_group" "backend" {
+  name        = "${substr(var.project_name, 0, 17)}-${var.environment}-tg5000"
+  port        = 5000
+  protocol    = "HTTP"
+  vpc_id      = var.vpc_id
+  target_type = "ip"
+
+  lifecycle {
+    create_before_destroy = true
+  }
+
+  health_check {
+    path                = "/api/health"
+    protocol            = "HTTP"
+    matcher             = "200-499"
+    interval            = 30
+    timeout             = 5
+    healthy_threshold   = 2
+    unhealthy_threshold = 3
+  }
+
+  tags = {
+    Name        = "${var.display_name}-${var.environment}-backend-tg"
+    Project     = var.display_name
+    Environment = var.environment
+  }
+}
+
 resource "aws_lb_listener" "http" {
   load_balancer_arn = aws_lb.main.arn
   port              = 80
@@ -48,5 +76,21 @@ resource "aws_lb_listener" "http" {
   default_action {
     type             = "forward"
     target_group_arn = aws_lb_target_group.frontend.arn
+  }
+}
+
+resource "aws_lb_listener_rule" "api" {
+  listener_arn = aws_lb_listener.http.arn
+  priority     = 10
+
+  action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.backend.arn
+  }
+
+  condition {
+    path_pattern {
+      values = ["/api/*"]
+    }
   }
 }
