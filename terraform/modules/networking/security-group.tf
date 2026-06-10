c5472fa0df01f57d8c171101d2dec0e7093cd0d7
mayorKHAY26 <mayor_omot@yahoo.com>
2026-06-10 16:11:02 -0400

    updated files

--- terraform/modules/networking/security-group.tf
+++ terraform/modules/networking/security-group.tf
@@ -37,8 +37,8 @@ resource "aws_security_group" "ecs" {
   }
 
   ingress {
-    from_port       = 3000
-    to_port         = 3000
+    from_port       = 80
+    to_port         = 80
     protocol        = "tcp"
     security_groups = [aws_security_group.alb.id]
     description     = "Frontend from ALB"
